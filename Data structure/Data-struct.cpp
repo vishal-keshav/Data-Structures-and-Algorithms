91b6df5aa1d434c0b73ec087b8c5cc9c5dd48d66
bulletcross <bulletcross@gmail.com>
2015-08-09 19:31:01 +0530

    Data structure upto binary search tree

---
+++ Data structure/Data-struct.cpp
@@ -0,0 +1,391 @@
+/* The motivation of creating this file is to construct every possible data structure and its use cases,
+     with minimal functionality and code lines, so that it can be easily implemented and used in case
+     of no availability of stl libraries provided by C++. Each data structure implemented is easy to
+     understand and implement on the go with minimum effort and time.*/
+
+/* Author-Vishal Keshav
+    email-bulletcross@gmail.com
+    www.github.com/bulletcross
+/*
+
+/* Data structure implemented are as follow:
+    1. Singly linked list
+    2. Doubly linked list
+    3. Stack
+    4. Queue
+    5. Heap
+    6. Binary Tree
+    7. Binary Search Tree
+    8. Balanced Binary Search Tree
+    9. Graph
+    10. Hash
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+//A generic data structure
+struct data{
+    int data1;
+    char data2;
+};
+
+//Singly Linked List start************************************************************
+struct sll_node{
+    data mydata;
+    sll_node * next;
+};
+//singly Linked List end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Doubly Linked List start***********************************************************
+struct dll_node{
+    data mydata;
+    dll_node *next;
+    dll_node *prev;
+};
+//Doubly Linked List end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Stack start*************************************************************************
+//Uses already created singly linked list structure
+void push(sll_node **head,data mydata){
+    sll_node *newnode = (sll_node *)malloc(sizeof(sll_node));
+    newnode->mydata=mydata;
+    newnode->next=(*head);
+    (*head)=newnode;
+}
+data pop(sll_node **head){
+    sll_node * newnode=(*head);
+    (*head)=newnode->next;
+    data mydata=newnode->mydata;
+    free(newnode);
+    return mydata;
+}
+//Stack end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Queue start**************************************************************************
+//Uses already created singly linked list structure
+void enq(sll_node **head,sll_node **tail,data mydata){
+    sll_node * newnode = (sll_node *)malloc(sizeof(sll_node));
+    newnode->mydata = mydata;
+    newnode->next=NULL;
+    if((*head)==NULL){
+        (*head)=newnode;
+        (*tail)=newnode;
+    }
+    else{
+        (*head)->next = newnode;
+        (*head)=newnode;
+    }
+}
+data deq(sll_node **tail){
+    sll_node *newnode = (*tail);
+    data mydata = newnode->mydata;
+    (*tail)=newnode->next;
+    free(newnode);
+    return mydata;
+}
+//Queue end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Heap Start*****************************************************************************
+    //Fixed size heap should be declared
+    //Defining functions for min heap
+    //This heapify is a non recursive efficient version and does not use unnecessary swap and child functions
+    void heapify(int *heap,int index,int current_size){
+        int left;
+        int right;
+        int min_index=index;
+        int temp;
+        do{
+            index=min_index;
+            left = 2*index +1;
+            right=2*index+2;
+            if(left<current_size && heap[min_index]>heap[left]){
+                min_index = left;
+            }
+            if(right<current_size && heap[min_index]>heap[right]){
+                min_index = right;
+            }
+            temp=heap[min_index];
+            heap[min_index]=heap[index];
+            heap[index]=temp;
+        }while(min_index!=index && min_index<current_size);
+    }
+    //current size will increase by one assuming all the checks have been done in main code
+    void heap_insert(int *heap,int key,int *current_size){
+        int index=(*current_size);
+        int parent=(index-1)/2;
+        int temp;
+        heap[index]=key;
+        (*current_size)=(*current_size)+1;
+        while(index>0 && heap[parent]>heap[index]){
+            temp=heap[parent];
+            heap[parent]=heap[index];
+            heap[index]=temp;
+            index=parent;
+            parent=(index-1)/2;
+        }
+    }
+    //Extracting minimum is based on assumptions that proper checks have been made for empty heap
+    int extract_min(int *heap,int *current_size){
+        int minimum=heap[0];
+        int index = (*current_size)-1;
+        (*current_size)=(*current_size)-1;
+        heap[0]=heap[index];
+        heapify(heap,0,index);
+        return minimum;
+    }
+//Heap end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Binary Tree Start************************************************************************
+    //Structure is very simple
+    struct tree_node{
+        data mydata;
+        tree_node *left;
+        tree_node *right;
+    };
+    //Tree traversal (preorder)
+    void preorder(tree_node *root){
+        if(root!=NULL){
+            printf("%d ",root->mydata.data1);
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
+    //Tree traversal (inorder)
+    void inorder(tree_node *root){
+        if(root!=NULL){
+            inorder(root->left);
+            printf("%d ",root->mydata.data1);
+            inorder(root->right);
+        }
+    }
+    //Tree traversal (post order)
+    void postorder(tree_node *root){
+        if(root!=NULL){
+            postorder(root->left);
+            postorder(root->right);
+            printf("%d ",root->mydata.data1);
+        }
+    }
+    //For random insertion in a tree
+    void tree_insert(tree_node **root,data mydata){
+        if((*root)==NULL){
+            (*root) = (tree_node *)malloc(sizeof(tree_node));
+            (*root)->mydata = mydata;
+            (*root)->left=NULL;
+            (*root)->right=NULL;
+        }
+        else{
+            int side = rand()%2;
+            if(side==0){
+                tree_insert(&((*root)->left),mydata);
+            }
+            else{
+                tree_insert(&((*root)->right),mydata);
+            }
+        }
+    }
+//Binary Tree end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Binary Search Tree**********************************************************************
+    //Structure used for binary tree is reused
+    void search_tree_insert(tree_node **root,data mydata){
+        if((*root)==NULL){
+            (*root)=(tree_node *)malloc(sizeof(tree_node));
+            (*root)->mydata = mydata;
+            (*root)->left=NULL;
+            (*root)->right=NULL;
+        }
+        else{
+            if((*root)->mydata.data1>mydata.data1){
+                search_tree_insert(&((*root)->left),mydata);
+            }
+            else{
+                search_tree_insert(&((*root)->right),mydata);
+            }
+        }
+    }
+    //Binary search returns node pointer
+    tree_node *search_tree_search(tree_node *root,data mydata){
+        tree_node *search_element=root;
+        if(search_element->mydata.data1==mydata.data1){
+            return search_element;
+        }
+        else{
+            if(mydata.data1<search_element->mydata.data1){
+                return search_tree_search(search_element->left,mydata);
+            }
+            else{
+                return search_tree_search(search_element->right,mydata);
+            }
+        }
+    }
+//Binary Search Tree end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Balanced Binary search tree*************************************************************
+
+//Balance Binary search tree end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int main(){
+/*
+    //Linked list check======================================
+    //If head is NULL, linked list is empty
+    sll_node * head=NULL;
+    //Entering elements in linked list
+    for(int i=0;i<5;i++){
+        sll_node * newnode = (sll_node *)malloc(sizeof(sll_node));
+        newnode->mydata.data1=i;
+        newnode->next = head;
+        head = newnode;
+    }
+    //Traversing list and removing element
+    sll_node *traverse=head;
+    sll_node *removal=traverse;
+    while(traverse!=NULL){
+        printf("%d ",traverse->mydata.data1);
+        traverse=traverse->next;
+        free(removal);
+        removal = traverse;
+    }
+    //Linked List check end++++++++++++++++++++++++++++++++++++
+*/
+/*
+    //Doubly Linked list check==================================
+    //If head is NULL, list is empty
+    //if head and tail are equal, then only one element is there.
+    dll_node *head=NULL;
+    dll_node *tail=head;
+    //Entering elements in linked list consecutively in head and tail.
+    //For doubly linked list, make sure to initialize for element and pointers of head and tail.
+    if(head==NULL && tail==NULL){
+        dll_node *newnode = (dll_node *)malloc(sizeof(dll_node));
+        newnode->next=NULL;
+        newnode->prev=NULL;
+        newnode->mydata.data1=0;
+        head=newnode;
+        tail=newnode;
+    }
+    for(int i=1;i<10;i++){
+        if(i%2==0){
+            dll_node *newnode = (dll_node *)malloc(sizeof(dll_node));
+            newnode->mydata.data1=i;
+            newnode->next=head;
+            newnode->prev=NULL;
+            head->prev=newnode;
+            head=newnode;
+        }
+        else{
+            dll_node *newnode = (dll_node *)malloc(sizeof(dll_node));
+            newnode->mydata.data1=i;
+            newnode->prev = tail;
+            newnode->next=NULL;
+            tail->next=newnode;
+            tail=newnode;
+        }
+    }
+    //Traversing list
+    dll_node *traverse_forward=head;
+    dll_node *traverse_backward=tail;
+
+    //While traversing, head and tail meets and if the crisscross, then special care has to be taken
+    while(!(traverse_forward->prev==traverse_backward || (traverse_forward->prev==traverse_backward->next && traverse_forward->prev!=NULL))){
+        printf("(%d - %d) ",traverse_forward->mydata.data1,traverse_backward->mydata.data1);
+        traverse_forward=traverse_forward->next;
+        traverse_backward=traverse_backward->prev;
+    }
+    //Doubly Linked List end++++++++++++++++++++++++++++++++++++++++
+*/
+
+/*
+    //Stack Check===============================================
+    //We need to manually check if stack is empty by equating it with NULL
+    sll_node *mystack=NULL;
+    //Pushing elements
+    for(int i=0;i<10;i++){
+        data mydata;
+        mydata.data1=i;
+        push(&mystack,mydata);
+    }
+    //Pop all the elements
+    while(mystack!=NULL){
+        data mydata = pop(&mystack);
+        printf("%d ", mydata.data1);
+    }
+    //Stack Check end+++++++++++++++++++++++++++++++++++++++++++++
+*/
+/*
+    //Queue Check================================================
+    //We need to manually handle head and tail of queue
+    sll_node *queue_head=NULL;
+    sll_node *queue_tail=NULL;
+    //Enqueue elements
+    //First eleme
+    for(int i=0;i<10;i++){
+        data mydata;
+        mydata.data1=i;
+        enq(&queue_head,&queue_tail,mydata);
+    }
+    //Dequeue all elements
+    //Condition for
+    while(queue_tail!=NULL){
+        data mydata = deq(&queue_tail);
+        printf("%d ",mydata.data1);
+    }
+    //Queue Check end+++++++++++++++++++++++++++++++++++++++++++++
+*/
+/*
+    //Min-heap Check==============================================
+    int *heap;
+    int heap_size=20;
+    int current_size=0;
+    //Declaring heap memory
+    heap = (int *)malloc(sizeof(int)*heap_size);
+    //Inserting random data
+    for(int i=0;i<20;i++){
+        int key = rand()%50;
+        heap_insert(heap,key,&current_size);
+    }
+    //Printing the elements and checking extract min and heapify function
+    int mini;
+    //Canont use current_size instead of 20 because its a dynamic variable
+    for(int i=0;i<20;i++){
+        for(int j=0;j<20-i;j++){
+            printf("%d ",heap[j]);
+        }
+        mini=extract_min(heap,&current_size);
+        printf("[%d]\n",mini);
+        //printf("\n");
+    }
+    //Min-heap Check end+++++++++++++++++++++++++++++++++++++++++++
+*/
+/*
+    //Binary tree check=============================================
+    //Construction of a random binary tree
+    tree_node *root=NULL;
+    data mydata;
+    for(int i=0;i<20;i++){
+        mydata.data1=rand()%100;
+        tree_insert(&root,mydata);
+    }
+    inorder(root);
+    //Binary tree check end+++++++++++++++++++++++++++++++++++++++++++++
+*/
+
+/*
+    //Binary search tree check===========================================
+    //Construction of random elements binary search tree
+    tree_node *root = NULL;
+    data mydata;
+    for(int i=0;i<20;i++){
+        mydata.data1=rand()%100;
+        search_tree_insert(&root,mydata);
+    }
+    //Inorder prints elements in sorted format
+    inorder(root);
+    //Binary search tree check end+++++++++++++++++++++++++++++++++++++++++
+*/
+
+    return 0;
+}
