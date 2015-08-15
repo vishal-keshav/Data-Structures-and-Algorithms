dbec2c306a6d65ed81c966468bb6b34e85fabeef
bulletcross <bulletcross@gmail.com>
2015-08-16 01:50:01 +0530

    algorithm file added

---
+++ Algorithms/Algo.cpp
@@ -0,0 +1,22 @@
+/*The motivation of creating this file is to touch every possible algorithm used in solving
+    problems that appear frequently in coding challanges and algorithm includes are not allowed.
+    The main aim is to prepare a exhaustive yet important implementation of algorithm in short way
+    such that re implementing such algorithm during challege environment dont take too much time.*/
+
+/*Author-Vishal Keshav
+    email-bulletcross@gmail.com
+    www.github.com
+*/
+
+/*Algorithms implemented
+    1. Binary search
+    2. n log(n) sort
+    3. n sort
+    4. Pattern matching
+    5. Backtracking
+    6. some Graph algorithm
+    7. some String algorithm
+    8. some Geometric algorithm
+    9. some bit algorithm
+*/
+
