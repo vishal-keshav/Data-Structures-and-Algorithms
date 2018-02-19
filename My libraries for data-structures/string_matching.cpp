521fc47383cb08257bf6c2ce5f0a0ecba586f10e
bulletcross <bulletcross@gmail.com>
2018-02-19 23:04:41 +0530

    LPS

--- My libraries for data-structures/string_matching.cpp
+++ My libraries for data-structures/string_matching.cpp
@@ -15,8 +15,48 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
- 
-//Following will be implemented
-// 1. Rabin Karp and general hashing strategy
-// 2. Maximum length prefix-suffix
-// 3. Knuth-morris-pratt algorithm
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+void LPS(string str, int *out, int n){
+	//Initialize first char with LPS 0
+	out[0] = 0;
+	int i=1,j=0;
+	//Iterate until all cells are done
+	while(i<n){
+		if(str[j] == str[i]){
+			out[i] = j+1;
+			i++;
+			j++;
+		}
+		else{
+			/*Find the next longest lps string ending at i-1
+			  By symmetry argument for lps until i-1, next
+			  lps can be found as lps[j-1]
+			*/
+			if(j==0){
+				out[i] = 0;
+				i++;
+			}
+			else{
+				j = out[j-1];
+			}
+		}
+	}
+	return;
+}
+
+int main(){
+	string sample = "ananabcbanana";
+	int *lps = new int[sample.size()];
+	LPS(sample, lps, sample.size());
+	for(int i=0;i<sample.size();i++){
+		cout << lps[i] << " ";
+	}
+
+	return 0;
+}
