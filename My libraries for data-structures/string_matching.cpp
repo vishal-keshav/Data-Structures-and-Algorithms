7e4c7b1f568a64004e07f0275cbf6871c3c39c7e
bulletcross <bulletcross@gmail.com>
2018-02-20 22:51:40 +0530

    added Rabin Karp

--- My libraries for data-structures/string_matching.cpp
+++ My libraries for data-structures/string_matching.cpp
@@ -50,6 +50,49 @@ void LPS(string str, int *out, int n){
 	return;
 }
 
+void search(char pat[], char txt[], int q)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+    int i, j;
+    int p = 0;
+    int t = 0;
+    int h = 1;
+ 
+    for (i = 0; i < M-1; i++)
+        h = (h*d)%q;
+ 
+    for (i = 0; i < M; i++)
+    {
+        p = (d*p + pat[i])%q;
+        t = (d*t + txt[i])%q;
+    }
+    for (i = 0; i <= N - M; i++)
+    {
+ 
+        if ( p == t )
+        {
+
+            for (j = 0; j < M; j++)
+            {
+                if (txt[i+j] != pat[j])
+                    break;
+            }
+
+            if (j == M)
+                printf("Pattern found at index %d \n", i);
+        }
+ 
+        if ( i < N-M )
+        {
+            t = (d*(t - txt[i]*h) + txt[i+M])%q;
+
+            if (t < 0)
+            t = (t + q);
+        }
+    }
+}
+
 int main(){
 	string sample = "ananabcbanana";
 	int *lps = new int[sample.size()];
